7551f4722a361d6de22eee9a2e0d4b9e6edf8ff7
Daniele Scasciafratte <mte90net@gmail.com>
2016-02-23 19:57:22 +0100

    improved apigen

--- apigen.sh
+++ apigen.sh
@@ -3,7 +3,7 @@ set -e
 
 slug="plugin-name"
 
-apigen --source=./ --exclude=*CMB*,*WP-Admin-Notice*,*WP-Contextual-Help*,*CPT_Core*,*Taxonomy_Core* --destination=./ --title="Wordpress Plugin Boilerplate Powered" > /dev/null
+apigen --source=./ --exclude=*CMB*,*WP-Admin-Notice*,*WP-Contextual-Help*,*CPT_Core*,*Taxonomy_Core*,*wp-background-processing*,*freemius*,*PointerPlus*,*CronPlus*,*WP-Contextual-Help*,*WP-Admin-Notice* --destination=./ --title="Wordpress Plugin Boilerplate Powered" > /dev/null
 
-echo "#menu > span:first-child, #menu > a:first-child span {background: url('https://raw.githubusercontent.com/Mte90/WordPress-Plugin-Boilerplate-Powered/master/plugin-name/assets/icon-128x128.png');content: "";height: 128px;width: 128px;display:block;}" >> ./resources/style.css
+echo "#menu > span:first-child, #menu > a:first-child span {background: url('https://raw.githubusercontent.com/Mte90/WordPress-Plugin-Boilerplate-Powered/master/plugin-name/assets/icon-256x256.png');content: '';height: 256px;width: 240px;display:block;margin: 0 auto;}" >> ./resources/style.css
 echo "#menu > span:first-child, #menu > a:first-child, #menu > a:first-child:hover {background-color:transparent;color: transparent;}" >> ./resources/style.css
