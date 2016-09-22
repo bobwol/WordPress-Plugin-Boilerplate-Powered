da06eb5195419f3dc838972ef0150d169629f1dc
Daniele Scasciafratte <mte90net@gmail.com>
2016-09-22 11:58:45 +0200

    updated documentation after last push in master

--- docs/elementlist.js
+++ docs/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["f","pl_my_register_widget()"],["c","Plugin_Name"],["c","Plugin_Name_Admin"],["c","Pn_ActDeact"],["c","Pn_CMB"],["c","Pn_ContextualHelp"],["c","Pn_Extras"],["f","pn_fs()"],["c","Pn_ImpExp"],["f","pn_load_plugin_textdomain()"],["c","Pn_My_Recent_Posts_Widget"],["c","Pn_P2P"],["c","Pn_Pointers"],["c","Pn_Uninstall"]];
+ApiGen.elements = [];
